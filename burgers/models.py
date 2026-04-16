4da31dece521c98ee2a6072c5e3958ce3f33dcbb
iamnotjungyo <kimass@swu.ac.kr>
2026-04-16 18:35:20 +0900

    fix: jungyo 브랜치 실습코드 삭제

--- burgers/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models # 장고가 가진 모듈 가져오기
-
-class Burger(models.Model) : # 햄버거를 나타내는 Model 클래스 정의 시작, 인수를 꼭 넣어야 Model 역할을 할 수 있음
-    name = models.CharField(max_length=20) # 문자열을 저장하는 CharField
-    price = models.IntegerField(default=0) # 숫자를 저장하는 IntegerField
-    calories = models.IntegerField(default=0) # 숫자를 저장하는 IntegerField
-
-    def __str__(self):
-        return self.name
--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from config.views import main, burger_list
-
-urlpatterns = [
-    path("admin/", admin.site.urls), # views.py에 작성한 main 함수 불러오기
-    path("", main), # 공백 (아무것도 입력하지 않은 경로)과 main 함수 연결
-    path("burgers/", burger_list),
-]
