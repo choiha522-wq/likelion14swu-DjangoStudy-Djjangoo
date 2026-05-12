53fb5d242ba6b0a0f3d4f20e37f5695e6f1c59aa
choiha522-wq <choiha522@gmail.com>
2026-05-13 03:32:20 +0900

    5ì£ ¼차 코드

---
+++ hyeona/class5/posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from posts.models import Post
+
+def feeds(request):
+
+    if not request.user.is_authenticated:
+        return redirect("/users/login/")
+
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "posts/feeds.html", context)
