e93b8e3aa2e2d55404b2e0eeb4cfea59942abae5
choiha522-wq <choiha522@gmail.com>
2026-06-03 02:06:39 +0900

    수정

--- hyeona/class7/config/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import redirect
-
-def index(request):
-    if request.user.is_authenticated:
-        return redirect("posts:feeds")
-    else:
-        return redirect("users:login")
