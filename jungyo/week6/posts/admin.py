d0d7b7e3da11a9e7b1a2effda2b02df1642ca40b
iamnotjungyo <kimass@swu.ac.kr>
2026-06-03 22:44:07 +0900

    jungyo: ~week7

---
+++ jungyo/week6/posts/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from posts.models import Post, PostImage, Comment
+import admin_thumbnails
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInLine(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInLine,
+    ]
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo",
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+    ]
---
+++ jungyo/week6/posts/views.py
@@ -0,0 +1,65 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from posts.forms import CommentForm, PostForm
+from django.shortcuts import render, redirect
+from posts.models import Post, Comment, PostImage
+from django.views.decorators.http import require_POST
+
+def feeds(request):
+
+    if not request.user.is_authenticated:
+        return redirect("users:login")
+
+    posts = Post.objects.all()
+    comment_form = CommentForm()
+    context = {
+        "posts": posts,
+    "comment_form": comment_form
+    }
+    return render(request, "posts/feeds.html", context)
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+
+        url = reverse("posts:feeds") + f"#post-{comment.post.id}"
+        return HttpResponseRedirect(url)
+
+@require_POST
+def comment_delete(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if comment.user == request.user:
+        comment.delete()
+        url = reverse("posts:feeds") + f"#post-{comment.post.id}"
+        return HttpResponseRedirect(url)
+    else:
+        return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
+
+def post_add(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+            for image_file in request.FILES.getlist("image"):
+                PostImage.objects.create(
+                    post=post,
+                    photo=image_file,
+                )
+
+            url = reverse("posts:feeds") + f"#post-{post.id}"
+            return HttpResponseRedirect(url)
+
+    form = PostForm()
+    context = {"form": form}
+    return render(request, "posts/post_add.html", context)
